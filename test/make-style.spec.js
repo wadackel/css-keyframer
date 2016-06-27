de07e0123c4cacfcfd9cd031892db4c38c77ea21
tsuyoshiwada <v23.b.wada@gmail.com>
2016-06-28 01:04:53 +0900

    Add makeStyle pretty tests

--- test/make-style.spec.js
+++ test/make-style.spec.js
@@ -1,28 +1,42 @@
 /* eslint-disable no-underscore-dangle, no-undefined */
 import assert from "power-assert";
-import makeStyles from "../src/make-style";
+import makeStyle from "../src/make-style";
 
 
-describe("makeStyles()", () => {
+describe("makeStyle()", () => {
   it("Should not be make style string", () => {
-    assert(makeStyles() == null);
-    assert(makeStyles("") == null);
-    assert(makeStyles(null) == null);
-    assert(makeStyles(undefined) == null);
-    assert(makeStyles(0) == null);
-    assert(makeStyles(123) == null);
-    assert(makeStyles([]) == null);
+    assert(makeStyle() == null);
+    assert(makeStyle("") == null);
+    assert(makeStyle(null) == null);
+    assert(makeStyle(undefined) == null);
+    assert(makeStyle(0) == null);
+    assert(makeStyle(123) == null);
+    assert(makeStyle([]) == null);
   });
 
   it("Should be make style string", () => {
-    assert(makeStyles(".hoge", {
+    assert(makeStyle(".hoge", {
       color: "#000",
       borderRadius: "50%",
       top: 0
     }) === ".hoge{color: #000;border-radius: 50%;top: 0;}");
 
-    assert(makeStyles(".hoge", {
+    assert(makeStyle(".hoge", {
       fuga: "hoge-fuga"
     }) === ".hoge{}");
+  });
+
+  it("Should be make pretty style string", () => {
+    assert(makeStyle(".fuga", {
+      width: "50px",
+      height: "50px",
+      "border-radius": "20%"
+    }, true) === [
+      ".fuga {",
+      "  width: 50px;",
+      "  height: 50px;",
+      "  border-radius: 20%;",
+      "}"
+    ].join("\n"));
   });
 });
